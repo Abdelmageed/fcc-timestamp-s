08e13de909cd2c24d57a0a69224e7be6adae8934
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-11 12:46:27 +0200

    favicon

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require ('express'),
+      serveFavicon = require ('serve-favicon'),
       path = require ('path'),
       app = express ()
 
 app.set ('view engine', 'pug')
+app.use (serveFavicon (path.join (__dirname, 'public/favicon.jpg')))
 app.get ('/', (req, res) => {
     res.render ('index')
 })
