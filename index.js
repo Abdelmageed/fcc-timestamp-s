e801666823577caff2e143047ca11285d50344de
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-11 12:50:05 +0200

    changed favicon extension from jpg -> ico

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require ('express'),
       app = express ()
 
 app.set ('view engine', 'pug')
-app.use (serveFavicon (path.join (__dirname, 'public/favicon.jpg')))
+app.use (serveFavicon (path.join (__dirname, 'public/favicon.ico')))
 app.get ('/', (req, res) => {
     res.render ('index')
 })
