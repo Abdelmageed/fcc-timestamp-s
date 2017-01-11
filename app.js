f6991e35edaae6f398f02cc6189f7b8fdec65f13
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-11 17:47:57 +0200

    passing natural date or a unix timestamp returns a json with the natural date and timestamp.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use (serveFavicon (__dirname + '/public/favicon.ico'))
 app.get ('/', (req, res) => {
     res.render ('index')
 })
+app.get ('/:time', (req, res) => {
+    let unix = parseInt(req.params.time, 10) * 1000,
+        date = new Date (unix || req.params.time)
+    res.json ({
+        unix: null || +(date.getTime () / 1000),
+        natural: (date.toDateString () === 'Invalid Date')? null : date.toDateString ()
+    })
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
