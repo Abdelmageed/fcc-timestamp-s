1515397378e85cd711f936e6c7faca1e92b0620e
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-11 12:57:43 +0200

    app listens to port supplied by heroku

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use (serveFavicon (path.join (__dirname, 'public/favicon.ico')))
 app.get ('/', (req, res) => {
     res.render ('index')
 })
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
